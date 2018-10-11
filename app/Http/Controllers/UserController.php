f11e5bc81769e39b408902dad8687b971b783aa7
subhabrata <subhabratakayal.cse@gmail.com>
2018-10-11 16:27:45 +0530

    changes on usercontroller

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -77,47 +77,60 @@ class UserController extends Controller
 					  $execute=1;
 					 }
 	       }else if($request->level==='08'){ //ppcell(WB13DTOC01/WB13DTHC01/WB13DTDEO001/WB13DTDEO002)
-                $userCreationType=$request->sub_level;
-                $ppcell_type=$request->ppcell;
+                  $userCreationType=$request->sub_level;
+                  $ppcell_type=$request->ppcell;
      			   if($userCreationType==='DT'){//PPCELL DISTRICT LEVEL USER CREATION
 
 						 if($ppcell_type!='DEO'){
-					    $user_id=$getStateCode.$UserArea.$userCreationType.$ppcell_type.'01';
-					    if(User::where('user_id', '=',$user_id)->exists()){
-					    $msg='User Already Exists';
-					    $execute=0;
-					    }else{
-					       $msg= $user_id;
-					       $execute=1;
-					      }
+						$user_id=$getStateCode.$UserArea.$userCreationType.$ppcell_type.'01';
+								
+								if(User::where('user_id', '=',$user_id)->exists()){
+								$msg='User Already Exists';
+									$execute=0;
+								}else{
+									$msg= $user_id;
+									$execute=1;
+								}
 						 }else{
-					    $subdiv_block_id=$ppcell_type;
-					    $user_code_like=$getStateCode.$UserArea.$userCreationType.$ppcell_type;
-				        $return=$this->getExistsSdoBdo_and_createDeo($user_code_like,$getStateCode,$UserArea,$userCreationType,$subdiv_block_id,$userGenertaionLevelCode='');
-					    $msg= $return['msg'];
-						$execute= $return['execute'];
+							 
+							$subdiv_block_id=$ppcell_type;
+							$user_code_like=$getStateCode.$UserArea.$userCreationType.$ppcell_type;
+							
+							$return=$this->getExistsSdoBdo_and_createDeo($user_code_like,$getStateCode,$UserArea,$userCreationType,$subdiv_block_id,$userGenertaionLevelCode='');
+						
+							$msg= $return['msg'];
+								$execute= $return['execute'];
 					 }
 				   }
                 if($userCreationType==='06'){//SDO LEVEL PPCELL USER CREATION
 						  $userCreationType="SDO";
 						  $subdiv_block_id=$request->subdiv_block_id;
 						  $ppcell_type=$request->ppcell;
+						  if(!empty($subdiv_block_id)){
 						  if($ppcell_type!='DEO'){
-					      $user_id=$getStateCode.$UserArea.$userCreationType.$subdiv_block_id.$ppcell_type.'01';
+                         
+					     $user_id=$getStateCode.$UserArea.$userCreationType.$subdiv_block_id.$ppcell_type.'01';
 					     if(User::where('user_id', '=',$user_id)->exists()){
 					     $msg='User Already Exists';
 					     $execute=0;
 					      }else{
 					       $msg= $user_id;
 					       $execute=1;
-					      }
+						  }
+						  
+
 						 }else{
 					      $user_code_like=$getStateCode.$UserArea.$userCreationType.$subdiv_block_id.'DEO';
 						  $userGenertaionLevelCode='DEO';
 						  $return=$this->getExistsSdoBdo_and_createDeo($user_code_like,$getStateCode,$UserArea,$userCreationType,$subdiv_block_id,$userGenertaionLevelCode);
 						  $msg= $return['msg'];
 						  $execute= $return['execute'];
-					     }
+						 }
+						}else{
+							$msg= 'Please choose subdivision from the dropdown';
+							$execute=0;
+
+						}
                      }
 
 
@@ -153,7 +166,7 @@ class UserController extends Controller
 		    }
 
 		if($execute!='' and $execute==1){
-
+			//return (print_r($request->all()));exit;
 			$AddUser=new User;
 			$AddUser->name = $request->name;
 			$AddUser->email = $request->email;
@@ -172,6 +185,7 @@ class UserController extends Controller
 			$AddUser->save();
 			$lastInsertedId=$AddUser->id; // get office id
 			//get office id
+	       //echo $user_type_code;exit;
 			$this->getDefaultMenuPermission_To_assignPermission($lastInsertedId,$user_type_code);
 			DB::table('user_random_password')->insert(
 				['rand_id' =>$msg  , 'rand_password' => $pass,'created_at'=>now()]
@@ -365,22 +379,22 @@ class UserController extends Controller
 
 	}
     public function getExistsSdoBdo_and_createDeo($user_code_like,$getStateCode,$UserArea,$userCreationType,$subdiv_block_id,$userGenertaionLevelCode){
-		$user= User::where('user_id', 'Like','%'.$user_code_like.'%')
-							->latest()->take(1)->get();
-
+		
+		$user= User::where('user_id', 'Like','%'.$user_code_like.'%')->latest()->take(1)->get();
+ // print_r($user);
 	    if(!empty($user[0]->user_id)){
-		   if(substr($user[0]->user_id,-1)!=4){
-			 $user_id=substr($user[0]->user_id,-1)+1;
-			   $msg=$getStateCode.$UserArea.$subdiv_block_id.$userGenertaionLevelCode.'0'.$user_id;
-			   $execute=1;
-			}else{
-			   $msg='Unable to process';
-			   $execute=0;
-			}
+				if(substr($user[0]->user_id,-1)!=4){
+					$user_id=substr($user[0]->user_id,-1)+1;
+					 $msg=$user_code_like.'0'.$user_id;
+					$execute=1;
+					}else{
+					$msg='Unable to process';
+					$execute=0;
+					}
 
-	       }else{
-			  $msg=$getStateCode.$UserArea.$subdiv_block_id.$userGenertaionLevelCode.'01';
-			  $execute=1;
+	       }else{     
+					  echo $msg=$user_code_like.'01';
+					$execute=1;
 	        }
 		  return array('msg'=>$msg,'execute'=>$execute);
 	}
